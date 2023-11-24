7e03ab951a45eaf22bbf6feb4e93e6a4361e1532
Johan Litsfeldt <johan.litsfeldt@uu.se>
2023-11-24 16:38:23 +0100

    Reset lastDisplayedChatId when event is changed

--- Core.cs
+++ Core.cs
@@ -58,6 +58,7 @@ namespace pp
 
         public static void SetEventTitle(string title) {
             eventTitle = title;
+            lastDisplayedChatId = -1;
         }
 
         public static string GetEventTitle()

673e9e3c569f90886278c602351558c854902e71
johlits <2831146+johlits@users.noreply.github.com>
2023-12-10 22:57:55 +0100

    Clarify download plugin description

--- Plugins/Download.cs
+++ Plugins/Download.cs
@@ -4,7 +4,7 @@ namespace pp.Plugins
     {
         public string GetDescription()
         {
-            return "Type /download <Plugin> to download a plugin.";
+            return "Type /download <plugin> to download a plugin.";
         }
 
         public bool ActivatesOn(string input)
